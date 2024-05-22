fb087061f97edac4b632c3095d1f35be5d8ccc38
jasmineeuy <jeuyoquea@gmail.com>
2024-05-21 21:01:44 -0400

    added file and completed classwork

--- create-react/src/App.js
+++ create-react/src/App.js
@@ -1,57 +1,76 @@
-const word = "Awesome";
-const fun = "Tech";
+import users from "./users.json";
+import todos from "./todos.json";
+console.log(users);
+console.log(todos);
 
-const num1 = 1000;
-const num2 = 100;
-
-const person = {
-  name: "Jasmine",
-  location: "Houston",
-  activity: ["cooking", "concert", "art"],
-};
-
-const cat = {
-  color: "tan",
-  numberOfKittens: 1,
-  img: "https://bestfriends.org/sites/default/files/inline-images/Foster-Agave-1-byAnnika-Harley.jpg",
-};
+const greeting = "Welcome to React";
+const user = "Cohort 2024";
 
 function App() {
   return (
     <div className="App">
       <h1>
-        {fun} is {word}
+        {greeting} {user}
       </h1>
-      <p>
-        Add: {num1} + {num2} = {num1 + num2}
-      </p>
-      <p>
-        Subtract: {num1} + {num2} = {num1 - num2}
-      </p>
-      <p>
-        {" "}
-        Multiply: {num1} * {num2} = {num1 * num2}
-      </p>
-      <p>
-        Divide: {num1} / {num2} = {num1 / num2}
-      </p>
-
-      <p>
-        Hi! My name is {person.name} and I live in {person.location}
-      </p>
-      <ul>
-        <li>{person.activity[0]}</li>
-        <li>{person.activity[1]}</li>
-        <li>{person.activity[2]}</li>
-      </ul>
-      <img src={cat.img} alt="img of grey cat with five kittens"></img>
-      <figcaption>
-        There is {cat.numberOfKittens} {cat.color} for fostering , they are cute
-        and love to play
-      </figcaption>
-      {/* This is how to comment*/}
+      <Users />
+      <Todos />
     </div>
   );
 }
 
+//create two functional components Users and TODOS
+
+// For each user
+// Their name in a <h3> element
+// The text "Email:" and their email in an anchor element (<a>). Use the proper attribute for emails
+// The text "Address:" and their address in an <address> element
+// Have the street address and suite number in one line and city and zipcode in another line
+// The text "Company:" and their company name and catchphrase
+function Users() {
+  return (
+    <div>
+      <h2>Users</h2>
+      {users.map((user) => (
+        <div>
+          <h3 key={user.name}>{user.name}</h3>
+          <p>
+            Email:
+            <a href={`mailto:${user.email}`}>{user.email}</a>
+          </p>
+          <p>Address:</p>
+          <address>
+            <p>
+              {user.address.street} {user.address.suite}
+            </p>
+            <p>
+              {user.address.city} {user.address.zipcode}
+            </p>
+          </address>
+          <p>Company:</p>
+          <p>{user.company.name}</p>
+          <p>{user.company.catchPhrase}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+//  A title for the whole component using an <h2> element
+// For each todo
+// The title of the todo
+// The text "Completed" if the value of the complete field is true, the text "In progress" otherwise.
+
+function Todos() {
+  return (
+    <div>
+      <h2>Todo</h2>
+      {todos.map((todo) => (
+        <div>
+          <p>Item: {todo.title}</p>
+          <p>Status:{todo.completed ? "Completed" : "In progress"}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 export default App;
