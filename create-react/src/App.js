3cd067b37b33f91c66efeadf7481c219fe1f0923
jasmineeuy <jeuyoquea@gmail.com>
2024-05-28 19:52:39 -0400

    state and hooks practice code

--- create-react/src/App.js
+++ create-react/src/App.js
@@ -1,41 +1,25 @@
-// # Create an array of strings named "items" at the top of the page before App component. Each string is an item in your shopping list. Build at least 3 items.
-import ShoppingListForm from "./components/ShoppingListForm";
-import ShoppingList from "./components/ShoppingList";
-const items = [
-  "mango",
-  "ice",
-  "chocolate",
-  "cheese",
-  "cookie",
-  "berry",
-  "taco",
-  "corn",
-];
-console.log(items);
+import React,{useState} from "react";
+
 
 function App() {
-  //handler functions to pass onto ShoppingListForm and ShoppingList via props
-  //   handleItemNameChange: Bind to input's onChange event
-  // handleFormSubmit: Bind to form's onSubmit event
-  // For each of these functions, add a parameter of event and just print the event to the console for now
+  const greeting = "Welcome to CodeSquad";
+  const [user,setUser] = useState("User");
+  const updateUsername =(event)=>{
+    setUser(event.target.value)
 
-  const handleItemNameChange = (event) => {
-    console.log(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    console.log(event.target.groceryItem.value);
-  };
+  }
   return (
     <div className="App">
-      <h1>Shopping List</h1>
-      <ShoppingListForm
-        handleItemNameChange={handleItemNameChange}
-        handleFormSubmit={handleFormSubmit}
+      <h1>
+        {greeting} {user}
+      </h1>
+      <label htmlFor="username">Name: </label>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        onChange={updateUsername}
       />
-      <ShoppingList items={items} />
-      {/*pass in data props to shopping list to use in shopping list component*/}
     </div>
   );
 }
